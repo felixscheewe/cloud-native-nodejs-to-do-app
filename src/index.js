b3f563c5cc97ced50d0775544be6ac98a4fe453f
felixscheewe <61014267+felixscheewe@users.noreply.github.com>
2023-11-05 13:50:57 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,32 +1,38 @@
+// Importieren der erforderlichen Module
 const express = require('express');
-const app = express();
-const db = require('./persistence');
-const getItems = require('./routes/getItems');
-const addItem = require('./routes/addItem');
-const updateItem = require('./routes/updateItem');
-const deleteItem = require('./routes/deleteItem');
+const app = express(); // Erstellt eine Express-Anwendung
+const db = require('./persistence'); // Importiert ein Modul zur Datenpersistenz
+const getItems = require('./routes/getItems'); // Importiert das Modul für das Abrufen von Items
+const addItem = require('./routes/addItem'); // Importiert das Modul für das Hinzufügen von Items
+const updateItem = require('./routes/updateItem'); // Importiert das Modul für das Aktualisieren von Items
+const deleteItem = require('./routes/deleteItem'); // Importiert das Modul für das Löschen von Items
 
-app.use(express.json());
-app.use(express.static(__dirname + '/static'));
+// Konfiguriert die Express-Anwendung
+app.use(express.json()); // Aktiviert das Parsen von JSON-Anfragen
+app.use(express.static(__dirname + '/static')); // Stellt statische Dateien im "static"-Verzeichnis bereit
 
-app.get('/items', getItems);
-app.post('/items', addItem);
-app.put('/items/:id', updateItem);
-app.delete('/items/:id', deleteItem);
+// Definiert Endpunkte für die RESTful API
+app.get('/items', getItems); // GET-Anfrage an /items ruft die Liste der Items ab
+app.post('/items', addItem); // POST-Anfrage an /items fügt ein neues Item hinzu
+app.put('/items/:id', updateItem); // PUT-Anfrage an /items/:id aktualisiert ein Item
+app.delete('/items/:id', deleteItem); // DELETE-Anfrage an /items/:id löscht ein Item
 
+// Initialisiert die Datenbank und startet den Server
 db.init().then(() => {
-    app.listen(3000, () => console.log('Listening on port 3000'));
+    app.listen(3000, () => console.log('Listening on port 3000')); // Startet den Server auf Port 3000
 }).catch((err) => {
     console.error(err);
-    process.exit(1);
+    process.exit(1); // Beendet die Anwendung bei einem Fehler während der Initialisierung
 });
 
+// Funktion für einen graceful Shutdown
 const gracefulShutdown = () => {
     db.teardown()
-        .catch(() => {})
-        .then(() => process.exit());
+        .catch(() => {}) // Ignoriert Fehler bei der Aufräumung
+        .then(() => process.exit()); // Beendet die Anwendung nach erfolgreicher Aufräumung
 };
 
-process.on('SIGINT', gracefulShutdown);
-process.on('SIGTERM', gracefulShutdown);
-process.on('SIGUSR2', gracefulShutdown); // Sent by nodemon
+// Registriert Handler für bestimmte Signale, um einen graceful Shutdown zu ermöglichen
+process.on('SIGINT', gracefulShutdown); // Signal bei manueller Beendigung (Strg+C)
+process.on('SIGTERM', gracefulShutdown); // Signal bei systematischer Beendigung
+process.on('SIGUSR2', gracefulShutdown); // Signal von nodemon, einem Tool zur automatischen Neustart der Anwendung
