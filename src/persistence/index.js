338e3320146dbccf7c5c00a687f1fdead8daba8b
felixscheewe <61014267+felixscheewe@users.noreply.github.com>
2023-11-05 13:54:42 +0100

    Update index.js

--- src/persistence/index.js
+++ src/persistence/index.js
@@ -1,2 +1,8 @@
-if (process.env.POSTGRES_HOST) module.exports = require('./postgres');
-else module.exports = require('./sqlite');
+// Überprüfe, ob die Umgebungsvariable POSTGRES_HOST gesetzt ist.
+if (process.env.POSTGRES_HOST) {
+  // Wenn POSTGRES_HOST gesetzt ist, importiere und exportiere das Modul './postgres'.
+  module.exports = require('./postgres');
+} else {
+  // Andernfalls, wenn POSTGRES_HOST nicht gesetzt ist, importiere und exportiere das Modul './sqlite'.
+  module.exports = require('./sqlite');
+}
