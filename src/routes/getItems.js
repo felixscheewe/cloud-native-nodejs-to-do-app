0001dde52d2d35aedf9bf75560586576e6eef4fd
felixscheewe <61014267+felixscheewe@users.noreply.github.com>
2023-11-05 13:59:35 +0100

    Update getItems.js

--- src/routes/getItems.js
+++ src/routes/getItems.js
@@ -1,6 +1,11 @@
+// Importiere das 'db'-Modul aus dem Verzeichnis '../persistence'
 const db = require('../persistence');
 
+// Exportiere eine asynchrone Funktion, die auf HTTP-Anfragen reagiert
 module.exports = async (req, res) => {
+    // Rufe die Funktion 'getItems' aus dem 'db'-Modul asynchron auf und warte auf das Ergebnis
     const items = await db.getItems();
+    
+    // Sende die zurückgegebenen 'items' als HTTP-Antwort zurück
     res.send(items);
 };
